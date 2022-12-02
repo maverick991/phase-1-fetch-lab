651201cb26668f27ca9e09fdac1a82a0907c2d55
MaverickEdwards <r.edwards0917@gmail.com>
2022-12-02 15:31:17 -0500

    commit tree ba816ae83dcbd0a05a4360de9ea6a95321ef9943

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((resp) => resp.json())
+  .then(resp => renderBooks(resp))
 }
 
 function renderBooks(books) {
@@ -8,7 +10,7 @@ function renderBooks(books) {
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
-    main.appendChild(h2);
+    main.appendChild(h2)
   });
 }
 
